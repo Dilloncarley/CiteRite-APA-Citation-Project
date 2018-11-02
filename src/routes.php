fb8bf714c9e2759171769fd556992ad77efd8fce
dcarley <dcarley@bomgar.com>
2018-11-02 13:27:46 -0500

    modified routes

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,3 @@
 <?php 
-require '../vendor/noodlehaus/dispatch/src/dispatch.php';
-$config = require __DIR__.'/config.php';
-$db = createDBConnection($config['db']);
+require '../vendor/autoload.php'
 ?>
