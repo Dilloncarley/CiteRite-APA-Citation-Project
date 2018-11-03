04a7e470236c867724a03d91d39ef446793b2cbe
dcarley <dcarley@bomgar.com>
2018-11-02 23:24:52 -0500

    new routes

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,18 @@
 <?php 
-require '../vendor/autoload.php'
+$app->get('/{name}', function ($request, $response, $args) {
+    $sql = "SELECT * FROM users";
+    $stmt = $this->db->query($sql);
+    $results = [];
+        while($row = $stmt->fetch()) {
+            $results[] = $row;
+        }
+    // $response->getBody()->write(var_export($results, true));
+    // return $response;
+    return $this->view->render($response, 'profile.html', [
+        'users' =>  $results
+    ]);
+})->setName('profile');
+$app->get('/', function ($request, $response) {
+    return $this->view->render($response, 'home.html');
+});
 ?>
