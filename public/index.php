cb56486a8afa88c6daaef9ba2af2d2785d4255fc
dcarley <dcarley@bomgar.com>
2018-11-02 23:24:12 -0500

    required files

--- public/index.php
+++ public/index.php
@@ -1,48 +1,31 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>CiteRite</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <?php 
+<?php 
     use \Psr\Http\Message\ServerRequestInterface as Request;
     use \Psr\Http\Message\ResponseInterface as Response;
     require 'vendor/autoload.php';
     require 'config/config.php';
-    require 'src/dependencies.php';
- 
+    if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) { return false; }
+
+    
+    //config app with settings
     $app = new \Slim\App(['settings' => $config]);
     $container = $app->getContainer();
-    $container['db'] = function ($c) {
-        $db = $c['settings']['db'];
-        $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
-            $db['user'], $db['pass']);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $pdo;
-    };
-    // Define app routes
-    $app->get('/hello/{name}', function ($request, $response, $args) {
-        // $records = $this->db->table('test')->where('name', 'like', '%foo%')->get();
+    
+    //App dependencies
+    require 'src/dependencies.php';
+
+    // App routes
+    require 'src/routes.php';
+ 
+    // $app->get('/hello/{name}', function ($request, $response, $args) {
+    //     // $records = $this->db->table('test')->where('name', 'like', '%foo%')->get();
 
         
         
-    $response->getBody()->write(var_export($records, true));
-    return $response;
-        // return $response->write("Hello " . $args['name']);
-    });
+    // $response->getBody()->write(var_export($records, true));
+    // return $response;
+    //     // return $response->write("Hello " . $args['name']);
+    // });
 
     // Run app
     $app->run();
 ?>
-    <body>
-   <h1>Hi</h1>
-    </body>
-</html>
