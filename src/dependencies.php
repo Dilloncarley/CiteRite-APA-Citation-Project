24086eb42374ea75487cc581683f2e7417d3181c
dcarley <dcarley@bomgar.com>
2018-11-02 23:24:34 -0500

    dep. file

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,4 +1,5 @@
 <?php
+//PDO db
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
     $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
@@ -7,4 +8,18 @@ $container['db'] = function ($c) {
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
+//twig templates
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('templates/', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
 ?>
